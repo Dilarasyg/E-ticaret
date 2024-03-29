91431426c12997864aa95f37484a3f49b094db59
Dilarasyg <116356960+Dilarasyg@users.noreply.github.com>
2024-03-29 12:08:08 +0300

    AdminLT dosyalrı yüklendi

---
+++ ASP.Net_E-Ticaret/ASP.Net_E-Ticaret/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_E_Ticaret.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.Net_E-Ticaret/ASP.Net_E-Ticaret/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.Net_E_Ticaret.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
